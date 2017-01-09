b9fecd13056945f0ea50a9cf7aebaf6843a4a95b
kurtw555 <kurt.wolfe@gmail.com>
2017-01-09 13:52:54 -0500

    Adding additional files.

---
+++ qed_hwbi/urls.py
@@ -0,0 +1,54 @@
+"""
+Definition of urls for qed_hwbi.
+"""
+
+from datetime import datetime
+from django.conf.urls import url
+import django.contrib.auth.views
+
+#import app.forms
+#import app.views
+import hwbi_app.views
+
+# Uncomment the next lines to enable the admin:
+# from django.conf.urls import include
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    url(r'^hwbi', hwbi_app.views.hwbi_view),
+    url(r'^$', description.description_page, {'model': "hwbi"}),
+    url(r'^description$', description.description_page, {'model': "hwbi"}),
+    url(r'^input$', input.input_page, {'model': "hwbi"}),
+    url(r'^map$', map.map_page, {'model': "hwbi"}),
+    url(r'^algorithms$', algorithms.algorithm_page, {'model': "hwbi"}),
+    url(r'^references$', references.references_page, {'model': "hwbi"}),
+    #url(r'^$', app.views.home, name='home'),
+    #url(r'^contact$', app.views.contact, name='contact'),
+    #url(r'^about', app.views.about, name='about'),
+    #url(r'^login/$',
+    #    django.contrib.auth.views.login,
+    #    {
+    #        'template_name': 'app/login.html',
+    #        'authentication_form': app.forms.BootstrapAuthenticationForm,
+    #        'extra_context':
+    #       {
+    #            'title': 'Log in',
+    #            'year': datetime.now().year,
+    #       }
+    #    },
+    #    name='login'),
+    #url(r'^logout$',
+    #    django.contrib.auth.views.logout,
+    #    {
+    #        'next_page': '/',
+    #    },
+    #    name='logout'),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
