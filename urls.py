04c5b97eae8287e6985d3068ed2c211f0adb2da2
AIgnatius <arignatius@gmail.com>
2017-01-13 16:36:31 -0500

    removed urls.py /

--- urls.py
+++ urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import include, url
 # the ^ in r'^cts/index.html$' indicates that we are looking to extend from the root dir from this part of the string
 # the $ in r'^cts/index.html$' indicates that we are looking to extend end the mathing part exactly here
 
-print('qud_hwbi.urls')
+print('qed_hwbi.urls')
 #appends to the list of url patterns to check against
 urlpatterns = [
     url(r'^hwbi/', include('hwbi_app.urls')),
